e1398614c48dc59f47ef9fa97dc81691f67ea926
titho1071 <tithoilham@gmail.com>
2025-03-12 15:14:55 +0700

    update 2

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/welcome', function () {
     return view('welcome');
 });
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID'. $id;
+});
