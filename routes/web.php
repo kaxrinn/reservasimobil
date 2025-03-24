90a25e2ec6c39d14e7000cb12c5ca0464a00a764
kaxrinn <kaxrinnberry@gmail.com>
2025-03-24 13:54:29 +0700

    tambah list barang

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListBarangController;
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
 Route::get('/user/{id}', function ($id) {
-    return 'User dengan ID' . $id;
+    return 'User dengan ID'. $id;
 });
+Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+    return view('list_barang', compact('id', 'nama'));
+});
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
